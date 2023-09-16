95ad70029468bc50c655b44ba0e242ac94b0d924
derpspy <aleks.arabadjiev05@gmail.com>
2023-09-16 19:22:29 +0300

    Upload Board-R part 1, an OOP task.

---
+++ C# Alpha/Alpha 54 C#/OOP/BoardR/BoardR 1/BoardR-Task-Organizing-System/Board-R/Board.cs
@@ -0,0 +1,7 @@
+﻿namespace Board_R.Models
+{
+    public static class Board
+    {
+        public static List<BoardItem> items = new List<BoardItem>();
+    }
+}
---
+++ C# Alpha/Alpha 54 C#/OOP/BoardR/BoardR 1/BoardR-Task-Organizing-System/Board-R/Models/Enums/Status.cs
@@ -0,0 +1,11 @@
+﻿namespace Board_R.Models.Enums
+{
+    public enum Status
+    {
+        Open,
+        Todo,
+        InProgress,
+        Done,
+        Verified
+    }
+}
